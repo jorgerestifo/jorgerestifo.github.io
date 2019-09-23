4b172f3471836b09f4da63fb2be8a2b82fe2c74c
jorger <jorge.restifo@rga.com>
2019-09-23 15:22:17 -0300

    mobile test

--- dist/bundle.js
+++ dist/bundle.js
@@ -8,65 +8,88 @@
 
     function loadAds() {
       for (var i = 0; i < urls.length; i++) {
-        var ifrm = document.createElement("iframe");
-        var img = document.createElement("IMG");
+        var ifrm = document.createElement("iframe"); // var img = document.createElement("IMG");
+
         var div = document.createElement("DIV");
         ifrm.setAttribute("src", "works/" + urls[i] + "/index.html");
-        ifrm.id = "ifrm" + i;
-        ifrm.style.display = "none";
-        img.src = "works/" + urls[i] + "/backup.jpg";
-        img.id = "img" + i; // img.style.transform= "scale(50%, 50%)";
-
-        loadImg(img, div, ifrm); // t.style.display="none";
+        ifrm.id = i;
+        ifrm.style.display = "none"; // img.src="works/"+urls[i]+"/backup.jpg";
+        // img.id="img"+i;
+        // img.style.transform= "scale(50%, 50%)";
+        // loadImg(img,div,ifrm);
+        // t.style.display="none";
 
         div.className = "ad";
-        div.addEventListener("mouseover", reloadAd); // div.appendChild(ifrm);
-        // div.appendChild(t);
+        ifrm.className = "ifrm"; // div.addEventListener("mouseover", reloadAd);
+
+        div.appendChild(ifrm); // div.appendChild(t);
 
         container.appendChild(div); // var temp = document.getElementById(i);
-        // iframeLoad(temp);
-      }
 
-      function loadImg(img, div, ifrm) {
-        img.addEventListener("load", function () {
-          var preloader = document.createElement("DIV");
-          div.style.width = img.width / 2 + "px";
-          div.style.height = img.height / 2 + "px";
-          div.className = "ad";
-          ifrm.style.width = img.width / 2 + "px";
-          ifrm.style.height = img.height / 2 + "px"; // div.addEventListener("mouseover", reloadAd);
-
-          div.appendChild(ifrm); // ifrm.style.display= "block";
-
-          div.appendChild(img); // container.appendChild(div);
-        });
-      } // function iframeLoad(iframe) {
-      //     iframe.onload = function () {
-      //         var metaData = iframe.contentDocument,
-      //         dimensionsStr = metaData.querySelector('meta[name="ad.size"]').content,
-      //         matched = dimensionsStr.match(/=(.*?),(.*?)=(.*?)$/),
-      //         width = parseInt(matched[1]),
-      //         height = parseInt(matched[3]);
-      //         iframe.style.width = width+"px";
-      //         iframe.style.height = height+"px";
-      //         // loader.style.display="none";
-      //         // iframe.style.display="inline-block";
-      //         // iframe.addEventListener("mouseover", reloadAd);
-      //         // setTimeout(reloadAd,15000,iframe)
-      //     }
+        iframeLoad(ifrm);
+      } // function loadImg(img,div,ifrm) {
+      //   img.addEventListener("load", function() {
+      //     // var preloader = document.createElement("DIV")
+      //     // div.style.width=img.width / 2 +"px";
+      //     // div.style.height=img.height / 2 +"px";
+      //     div.className = "ad"
+      //     // ifrm.style.width=img.width / 2 +"px";
+      //     // ifrm.style.height=img.height / 2 +"px";
+      //     // div.addEventListener("mouseover", reloadAd);
+      //     div.appendChild(ifrm);
+      //     // ifrm.style.display= "block";
+      //     div.appendChild(img);
+      //     // container.appendChild(div);
+      //   });
       // }
 
+
+      function iframeLoad(iframe) {
+        iframe.onload = function () {
+          var metaData = iframe.contentDocument,
+              dimensionsStr = metaData.querySelector('meta[name="ad.size"]').content,
+              matched = dimensionsStr.match(/=(.*?),(.*?)=(.*?)$/),
+              width = parseInt(matched[1]),
+              height = parseInt(matched[3]);
+          iframe.style.width = width + "px";
+          iframe.style.height = height + "px";
+          iframe.style.display = "block";
+          iframe.style.opacity = "0";
+
+          if (iframe.id < 1) {
+            iframe.style.opacity = "1";
+          } // loader.style.display="none";
+          // iframe.addEventListener("mouseover", reloadAd);
+          // setTimeout(reloadAd,15000,iframe)
+
+        };
+      }
+
+      $(function () {
+        $(".ifrm").recliner({
+          attrib: "src",
+          // selector for attribute containing the media src
+          throttle: 100,
+          // millisecond interval at which to process events
+          threshold: 25,
+          // scroll distance from element before its loaded
+          live: true // auto bind lazy loading to ajax loaded elements
+
+        });
+      }); // $(document).on('lazyloded', '.ifrm', function() {
+      // var $e = $(this);
+      // // do something with the loaded element...
+      // console.log('lazyshow', $e);
+      // TweenMax.from($e,1,{autoAlpha:0})
+      // });
+      // // $(document).on('lazyload', '.ifrm', function() {
+      // // var $e = $(this);
+      // // // do something with the loaded element...
+      // // console.log('lazyshow', $e);
+      // // });
     }
 
     loadAds();
-
-    function reloadAd(e) {
-      e.currentTarget.childNodes[0].style.display = "block";
-      e.currentTarget.childNodes[1].style.display = "none";
-      e.currentTarget.childNodes[0].contentWindow.location.reload(); // console.log(e.currentTarget);
-      // e.target.childNodes[0].contentWindow.location.reload();
-      // setTimeout(reloadAd,2000,iframe);
-    } // function showAd() {
     //     var elem = document.querySelector('#ad');
     //     if(elem!=null) elem.parentNode.removeChild(elem);
     //     var random = Math.floor(Math.random()*urls.length);
